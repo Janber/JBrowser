08958233f7c03c5a25f9c38a17d2232f62788bbc
Jumbo <kurekenpa@gmail.com>
2015-11-23 21:08:52 +0900

    origin

---
+++ JBrowser/JBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JBrowser
+//
+//  Created by JW on 11/23/27 H.
+//  Copyright © 27 Heisei Woo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
